b4f05be96b63621d3de8065e943304852640b688
Lucas Welscher <lucas.welscher@tum.de>
2022-05-15 21:35:14 +0200

    Exercise-Template pushed by Artemis

---
+++ test/de/tum/in/ase/eist/EnrollmentServiceTest.java
@@ -0,0 +1,28 @@
+package de.tum.in.ase.eist;
+
+import static org.easymock.EasyMock.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.easymock.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(EasyMockExtension.class)
+class EnrollmentServiceTest {
+
+	// TODO 1: setup EnrollmentServiceTest with all necessary attributes
+
+	@Test
+	void testEnrollStudentSuccessful() {
+
+		// TODO 2: implement the test
+		fail("not implemented yet");
+	}
+
+	@Test
+	void testEnrollStudentFailure() {
+
+		// TODO 3: Implement the test (optional)
+	}
+
+}
