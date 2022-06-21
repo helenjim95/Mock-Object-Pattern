f2fa928a995e2b62a0e7f394d18b0c3e40976eec
helenjim49 <helenjim49@gmail.com>
2022-06-21 16:53:21 +0200

    init

--- test/de/tum/in/ase/eist/EnrollmentServiceTest.java
+++ test/de/tum/in/ase/eist/EnrollmentServiceTest.java
@@ -10,19 +10,33 @@ import org.junit.jupiter.api.extension.ExtendWith;
 @ExtendWith(EasyMockExtension.class)
 class EnrollmentServiceTest {
 
-	// TODO 1: setup EnrollmentServiceTest with all necessary attributes
+	// 1. setup EnrollmentServiceTest with all necessary attributes
+	@TestSubject
+	private EnrollmentService enrollmentService = new EnrollmentService();
+	@Mock
+	private Course courseMock;
 
 	@Test
 	void testEnrollStudentSuccessful() {
-
-		// TODO 2: implement the test
-		fail("not implemented yet");
+		// 2: implement the test
+		Student student = new Student();
+		int expectedSize = student.getCourses().size() + 1;
+		expect(courseMock.enroll(student)).andReturn(true);
+		replay(courseMock);
+		enrollmentService.enroll(student, courseMock);
+		assertEquals(expectedSize, student.getCourses().size());
 	}
 
 	@Test
 	void testEnrollStudentFailure() {
 
-		// TODO 3: Implement the test (optional)
+		// 3: Implement the test (optional)
+		Student student = new Student();
+		int expectedSize = student.getCourses().size();
+		expect(courseMock.enroll(student)).andReturn(false);
+		replay(courseMock);
+		enrollmentService.enroll(student, courseMock);
+		assertEquals(expectedSize, student.getCourses().size());
 	}
 
 }
