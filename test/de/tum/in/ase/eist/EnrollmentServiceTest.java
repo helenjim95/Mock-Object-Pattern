e648fd18d56ed2636ef75b4355bb48582bb9cd0e
ge32goy <hanyin.chen@tum.de>
2022-07-23 16:17:55 +0200

    init

--- test/de/tum/in/ase/eist/EnrollmentServiceTest.java
+++ test/de/tum/in/ase/eist/EnrollmentServiceTest.java
@@ -1,16 +1,19 @@
 package de.tum.in.ase.eist;
 
+//TODO: import EasyMock
 import static org.easymock.EasyMock.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+//TODO: import EasyMock
 import org.easymock.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+//TODO: Easymock annotation
 @ExtendWith(EasyMockExtension.class)
 class EnrollmentServiceTest {
 
-	// 1. setup EnrollmentServiceTest with all necessary attributes
+	// TODO: 1. setup EnrollmentServiceTest with all necessary attributes
 	@TestSubject
 	private EnrollmentService enrollmentService = new EnrollmentService();
 	@Mock
@@ -18,25 +21,32 @@ class EnrollmentServiceTest {
 
 	@Test
 	void testEnrollStudentSuccessful() {
-		// 2: implement the test
+		// TODO: 2: implement the test
+//      TODO: dummy object to be passed as parameter
 		Student student = new Student();
 		int expectedSize = student.getCourses().size() + 1;
+//		TODO: specify the behavior -> hardcode output of a method call
 		expect(courseMock.enroll(student)).andReturn(true);
+//		TODO: make the Mock ready to play
 		replay(courseMock);
+//		TODO: test the method of the system enrollmentService with dummy/mock
 		enrollmentService.enroll(student, courseMock);
+//		TODO: assert the expected and observed output
 		assertEquals(expectedSize, student.getCourses().size());
+//		TODO: check if the Mock has been called in the test (optional)
+		verify(courseMock);
 	}
 
 	@Test
 	void testEnrollStudentFailure() {
-
-		// 3: Implement the test (optional)
+		// TODO: 3: Implement the test
 		Student student = new Student();
 		int expectedSize = student.getCourses().size();
 		expect(courseMock.enroll(student)).andReturn(false);
 		replay(courseMock);
 		enrollmentService.enroll(student, courseMock);
 		assertEquals(expectedSize, student.getCourses().size());
+		verify(courseMock);
 	}
 
 }
